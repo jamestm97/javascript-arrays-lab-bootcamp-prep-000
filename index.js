7d45aa3de012a437fd147ffed0a9c13e65a6a01e
James McKay <jamestm97@gmail.com>
2017-09-26 21:34:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,8 @@ function destructivelyRemoveFirstKitten() {
   kittens.shift();
   return kittens;
 }
+
+function appendKitten(name) {
+  var lastKitten = [...kittens, name];
+  return lastKitten;
+}
